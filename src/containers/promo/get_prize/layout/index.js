51343f89b0fb173e2e85b792bc86295e3513f190
admin <admin@admins-MBP.Dlink>
2019-02-22 17:46:52 +0500

    получение приза карты центр

--- src/containers/promo/get_prize/layout/index.js
+++ src/containers/promo/get_prize/layout/index.js
@@ -3,18 +3,16 @@ import {FlatList,ImageBackground,Keyboard,ScrollView,Text,TouchableOpacity,View,
 import EStyleSheet from 'react-native-extended-stylesheet';
 import {withNavigation} from 'react-navigation';
 
-import Input		from '../../../../templates/input';
-import DateInput	from '../../../../templates/input_date';
-import SelectAdress	from '../../../../templates/select_adress';
-import SubTitle		from '../../../../templates/subtitle';
+import Input			from '../../../../templates/input';
+import SelectCenter	from '../../../../templates/select_center';
 
 const styles = EStyleSheet.create({
 	container: {
 		backgroundColor: '#fff',
-		paddingHorizontal: 20,
 	},
 	fio_area: {
-		paddingTop: 20, paddingBottom: 10,
+		paddingHorizontal: 20,
+		paddingTop: 20, paddingBottom: 15,
 	},
 	fio_text: {
 		color: '#3d3d3d',
@@ -26,23 +24,17 @@ const styles = EStyleSheet.create({
 	block: {
 		marginBottom: 10,
 	},
-	adress_title: {
-		paddingHorizontal: 20,
-	},
-	adress_text: {
-		color: '#3d3d3d',
-		fontSize: 14, lineHeight: 18,
-		fontFamily: 'GothamPro',
-		textAlign: 'center',
-		paddingBottom: 8,
-		marginTop: 20, marginBottom: 25,
+	map: {
+		backgroundColor: '#ccc',
+		width: 320, height: 250,
 	},
 	save: {
-		backgroundColor: '$red',
 		alignItems: 'center',
-		marginBottom: 30,
 		padding: 15,
+		marginHorizontal: 20,
+		marginTop: 20, marginBottom: 30,
 		borderRadius: 100,
+		backgroundColor: '$red',
 	},
 	save_text: {
 		color: '#fff',
@@ -51,179 +43,240 @@ const styles = EStyleSheet.create({
 	}
 });
 
-
-export default withNavigation(class GetPrize extends Component {
+const options = [
+	{
+		title: 'kenya',
+		id: 1,
+	},
+	{
+		title: 'uganda',
+		id: 2,
+	},
+	{
+		title: 'libya',
+		id: 3,
+	},
+	{
+		title: 'morocco',
+		id: 4,
+	},
+];
+export default withNavigation(class GetPrizeCenterLayout extends Component {
 	constructor(props) {
 		super(props);
-		props = props.user;
-		console.log('constructor',this)
-		this.state = {
-			name: 		props.name ??  '',
-			father: 	props.father ?? '',
-			family: 	props.family ?? '',
-			mail: 		props.mail ?? '',
-			birthday: 	props.birthday ?? '',
-			adress: 	props.adress.complete ?? '',
 
-			name_error:   	false,
-			father_error: 	false,
-			family_error: 	false,
-			mail_error:	 	false,
-			birthday_error:	false,
-			adress_error: 	false,
-			// если данные уже были в хранилище, то делаем поле нередактируемым
-			name_editable: 	 !props.name,
-			father_editable: !props.father,
-			family_editable: !props.family,
-			mail_editable:	 !props.mail,
+		this.scroll = React.createRef();
+		this.inputs = {
+			name: {
+				ref: React.createRef(),
+				offset: 0,
+			},
+			father: {
+				ref: React.createRef(),
+				offset: 0,
+			},
+			family: {
+				ref: React.createRef(),
+				offset: 0,
+			},
+			mail: {
+				ref: React.createRef(),
+				offset: 200*EStyleSheet.value("$scale"),
+			},
+			center: {
+				ref: React.createRef(),
+				offset: 300*EStyleSheet.value("$scale"),
+			},
+		};
+
+		this.required = ['name','father','family','mail','center'];
+
+		this.state = {
+			...props.user,
+
+			center: '',
+			mail: 'xss@mail.ru',
+
+			name_error:   	'',
+			father_error: 	'',
+			family_error: 	'',
+			mail_error:	 	'',
+			center_error: 	'',
+
+			waiting: false,
+			ready: false,
+
+			button_state: 'ready',
 		};
 	}
 
 	componentDidUpdate(prev_props) {
+		// Изменились данные о пользователе
 		if(!Object.is(prev_props.user,this.props.user)) {
-			let props = this.props.user;
-			let state = this.state;
-			console.log('componentDidUpdate_this',this)
-			this.setState({
-				name: 			 props.name ?? state.name ?? '',
-				father: 		 props.father ?? state.father ?? '',
-				family: 		 props.family ?? state.family ?? '',
-				mail: 			 props.mail ?? state.mail ?? '',
-				birthday: 		 props.birthday ?? state.birthday ?? '',
-				adress: 		 props.adress.complete ?? state.adress ?? '',
+			this.setState(this.props.user);
+		}
 
-				name_editable: 	 !props.name,
-				father_editable: !props.father,
-				family_editable: !props.family,
-				mail_editable:	 !props.mail,
-			})
-
-			// убираем ошибки, если на других страницах пользователь ввел данные
-			let fields = ['name', 'father', 'family', 'mail', 'birthday', 'adress'];
-			fields.forEach(field => {
-				if (props[field].length && state[field + '_error']) {
-					this.setState({ [field + '_error']: false });
-				}
-			})
+		// Изменилось состояние запроса
+		if(prev_props.state != this.props.state) {
+			if(this.props.state == 'waiting') this.setState({button_state:'waiting'});
+			if(this.props.state == 'succeed') this.setState({button_state:'end'});
+			if(this.props.state == 'errored') this.setState({button_state:'ready'});
 		}
 	}
 
-	update = async (data) =>{
+	update = async (data) => {
 		await this.setState(data);
-		// убираем ошибки
-		let state = this.state;
-		let fields = ['name','father','family','mail','birthday'];
-		fields.forEach(field =>{
-			if(state[field].length && state[field+'_error']){
-				this.setState({[field+'_error']:false});
-			}
-		})
+
+		// Убираем ошибки
+		if(this.state.name.length		&& this.state.name_error.length)		this.setState({name_error:false});
+		if(this.state.father.length		&& this.state.father_error.length)		this.setState({father_error:false});
+		if(this.state.family.length		&& this.state.family_error.length)		this.setState({family_error:false});
+		if(this.state.mail.length		&& this.state.mail_error.length)		this.setState({mail_error:false}); // потом нормальную надо сделать
+		if(this.state.center.length		&& this.state.center_error.length)		this.setState({center_error:false});
 	}
 
-	check_completeness = () =>{
-		// Проверяем поля
+	// Проверяем, готова ли форма
+	check_ready = async () => {
+		await this.setState(state => ({
+			ready: this.required.every(field => state[field].length)
+		}));
+	}
+
+	// Указание на ошибки при заполнении полей
+	check_completeness = async () => {
 		let state = this.state;
+
+		await this.check_ready();
+
+		// Проверяем поля
 		let fields = [
 			{
 				field: 'name',
-				error:  'Введите имя'
+				error: 'Введите имя'
 			},
 			{
 				field: 'father',
-				error:  'Введите отчество'
+				error: 'Введите отчество'
 			},
 			{
 				field: 'family',
-				error:  'Введите фамилию'
+				error: 'Введите фамилию'
 			},
 			{
 				field: 'mail',
-				error:  'Введите почтоый ящик'
+				error: 'Укажите почтоый ящик'
 			},
 			{
-				field: 'birthday',
-				error:  'Введите день рождения'
+				field: 'center',
+				error: 'Выберите центр выдачи'
 			},
-			{
-				field: 'adress',
-				error:  'Введите адрес доставки'
-			},
-		]
-		return fields.every(item =>{
+		];
+
+		return fields.every(item => {
 			let {field,error} = item;
 			if(!state[field]?.length){
 				this.setState({[field+'_error']:error});
-				//this.props.scroll.current.scrollTo({y:this.inputs.field.offset});
+				this.scroll.current.scrollTo({y:this.inputs[field].offset});
 				return false;
-			} else{
+			} else {
 				this.setState({[field+'_error']:false});
 				return true;
 			}
 		})
 	}
 
-	send = () =>{
+	send = async () => {
 		let state = this.state;
 		Keyboard.dismiss();
 
-		if (!this.check_completeness()) return;
+		// Проверяем все поля
+		if(!await this.check_completeness()) return;
+
+		// Отправляем изменения
+		await this.setState({waiting:true});
+		await this.props.send_data({
+			name:   	state.name,
+			father: 	state.father,
+			family: 	state.family,
+			mail:	 	state.mail,
+			center: 	state.center,
+		});
+		await this.setState({waiting:false});
 	}
 
 	render() {
-		let {state,props} = this;
-		console.log('render_this',this)
+		let {props,state} = this;
+		console.log(state);
+
 		return (
-			<ScrollView style={styles.container}>
+			<ScrollView ref={this.scroll} keyboardShouldPersistTaps="always" keyboardDismissMode="on-drag" style={styles.container}>
 				<View style={styles.fio_area}>
 					<View style={styles.block}>
-						<Text style={styles.fio_text}>Заполните форму для получения выигрыша почтой.</Text>
+						<Text style={styles.fio_text}>Заполните форму для получения выигрыша в Центре выдачи призов.</Text>
 						<Input
+							id={this.inputs.name.ref}
 							title="Имя"
 							value={state.name}
-							update={value => this.update({ name: value })}
+							disabled={props.user.name.length}
+							update={name => this.update({name})}
 							error={state.name_error}
-							editable={state.name_editable}
+							keyboard_options={{
+								scroll: this.scroll,
+								offset: this.inputs.name.offset,
+							}}
 						/>
 						<Input
+							id={this.inputs.father.ref}
 							title="Отчество"
 							value={state.father}
-							update={value => this.update({ father: value })}
+							disabled={props.user.father.length}
+							update={father => this.update({father})}
 							error={state.father_error}
-							editable={state.father_editable}
+							keyboard_options={{
+								scroll: this.scroll,
+								offset: this.inputs.father.offset,
+							}}
 						/>
 						<Input
+							id={this.inputs.family.ref}
 							title="Фамилия"
 							value={state.family}
-							update={value => this.update({ family: value })}
+							disabled={props.user.family.length}
+							update={family => this.update({family})}
 							error={state.family_error}
-							editable={state.family_editable}
+							keyboard_options={{
+								scroll: this.scroll,
+								offset: this.inputs.family.offset,
+							}}
 						/>
 					</View>
 					<View style={styles.block}>
 						<Input
+							id={this.inputs.mail.ref}
 							title="E-mail"
 							value={state.mail}
-							update={value => this.update({ mail: value })}
-							error={state.mail_error}
 							type="email-address"
-							editable={state.mail_editable}
+							disabled={props.user.mail.length}
+							update={mail => this.update({mail})}
+							error={state.mail_error}
+							keyboard_options={{
+								scroll: this.scroll,
+								offset: this.inputs.mail.offset,
+							}}
 						/>
-						<DateInput
-							title="Дата рождения"
-							value={state.birthday}
-							update={value => this.update({ birthday: value })}
-							error={state.birthday_error}
+						<SelectCenter
+							title="Выберите центр выдачи"
+							value={state.center}
+							data={options}
+							update={center => this.update({center})}
+							error={state.center_error}
 						/>
 					</View>
 				</View>
-				<View style={styles.adress_area}>
-					<SubTitle style={styles.adress_title} text="Адрес доставки" />
-					<SelectAdress
-						title="Укажите адрес доставки"
-						value={state.adress}
-						error={state.adress_error}
-					/>
-					<Text style={styles.adress_text}>Текст, описывающий особенности получения выигрыша, правила и какие-то хитрости. Может занимать несколько строчек.</Text>
+				<View style={styles.map_area}>
+					<View style={styles.map}>
+						{/*  */}
+					</View>
 					<TouchableOpacity style={styles.save} onPress={this.send}>
 						<Text style={styles.save_text}>Отправить</Text>
 					</TouchableOpacity>
